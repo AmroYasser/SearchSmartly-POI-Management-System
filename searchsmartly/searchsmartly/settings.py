42e065d56076ef3421f74ff67aadad8ddfdf708d
Amr Yasser <3mryasser97@gmail.com>
2025-09-03 16:12:18 +0300

    Configure Django to use PostgreSQL with PostGIS

--- searchsmartly/searchsmartly/settings.py
+++ searchsmartly/searchsmartly/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'searchsmartly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'searchsmartly',
+        'USER': 'searchsmartly',
+        'PASSWORD': 'searchsmartly',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
