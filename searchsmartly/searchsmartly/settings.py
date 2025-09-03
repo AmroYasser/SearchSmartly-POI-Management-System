d7b895348ba0aa82cc32267e4dd564541329dc47
Amr Yasser <3mryasser97@gmail.com>
2025-09-03 16:16:49 +0300

    register geodjango

--- searchsmartly/searchsmartly/settings.py
+++ searchsmartly/searchsmartly/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # geodjango
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
