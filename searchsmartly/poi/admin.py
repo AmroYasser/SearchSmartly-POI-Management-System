cddac405dce8a82dff9e52f73755e8d7b71a7482
Amr Yasser <3mryasser97@gmail.com>
2025-09-03 16:48:50 +0300

    create the django admin interface

--- searchsmartly/poi/admin.py
+++ searchsmartly/poi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
+from .models import PoI
 
-# Register your models here.
+@admin.register(PoI)
+class PoIAdmin(GISModelAdmin):
+    list_display = ('internal_id', 'name', 'external_id', 'category', 'avg_rating')
+    search_fields = ('internal_id', 'external_id')
+    list_filter = ('category',)
+
+    def avg_rating(self, obj):
+        return sum(obj.ratings) / len(obj.ratings)
